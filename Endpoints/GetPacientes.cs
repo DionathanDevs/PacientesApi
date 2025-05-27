f0baab46286c5b56927c3d8e7372b2a64639c22c
Dionathan Boing Mesquita <dionathandev@gmail.com>
2025-05-27 17:33:10 -0300

    Update GetPacientes.cs

--- Endpoints/GetPacientes.cs
+++ Endpoints/GetPacientes.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using PacientesApi.Data;
-using PacientesApi.Models; // Para a classe Paciente
+using PacientesApi.Models; 
 
 
 namespace PacientesApi.Endpoints;
